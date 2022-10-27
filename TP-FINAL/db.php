51e7ae6a52bbbf7229e2ea252acce07809c473b6
ILoOeL <115983938+ILoOeL@users.noreply.github.com>
2022-10-27 10:34:42 +0200

    Add files via upload

--- TP-FINAL/db.php
+++ TP-FINAL/db.php
@@ -1,12 +1,11 @@
+
 <?php
-   $con=mysqli_connect('127.0.0.7','root','root','tp_final');
+// CONNECTION AVEC LA BASE DE DONNEES ET GESTION DES ERREURS
+   $con=mysqli_connect('127.0.0.7','root','root','tp_final'); //$con variable pour ce connecter défini par mysql_connect (puis les données)
    if(!$con){
-      die('erreur de connexion a la base de donnée');
+      die('erreur de connexion a la base de donnée'); // si non accès aux données = erreur 
    }else{
       }
-   $pdo= new PDO('mysql:dbname=tp_final;host=localhost','root','root');
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   
-   // ICI DOIT APPARAITRE LA CONNECTION AVEC LA BASE DE DONNEES
-   // ET LA GESTION DES ERREURS
+   $pdo= new PDO('mysql:dbname=tp_final;host=localhost','root','root'); //sinon connection à la bdd (PDO, PHP data object) extension orienté objet
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // gestion des erreurs
 ?>
